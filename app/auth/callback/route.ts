2d25ca8508366aed88bd7aa4c20eeb37006746d2
nadhifhafizp <nadhifhafiz@gmail.com>
2025-12-04 06:49:23 +0700

    add route auth

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,30 @@
+import { createClient } from '@/lib/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
+  // "next" adalah halaman tujuan setelah login (misal: /dashboard/settings)
+  const next = searchParams.get('next') ?? '/dashboard'
+
+  if (code) {
+    const supabase = await createClient()
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    
+    if (!error) {
+      const forwardedHost = request.headers.get('x-forwarded-host') // Penting untuk Vercel
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
+    }
+  }
+
+  // Jika error, kembalikan ke login
+  return NextResponse.redirect(`${origin}/login?error=auth-code-error`)
+}
